58fb4e3528c55e404b0500c0dcbe30fb403ee26e
Sergios Lenis <sergioslenis@gmail.com>
2015-11-10 23:56:51 +0200

    Init

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,13 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses= ''
+    @wrong_guesses=''
+
   end
 
   def self.get_random_word
@@ -19,4 +23,50 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess(guess)
+    if guess=='' || guess.nil? ||  guess =~ /\W/
+      raise ArgumentError.new()
+    end
+    guess=guess.downcase
+    # if guess
+    if  @guesses.include? guess or @wrong_guesses.include? guess
+      return false
+    # end
+    elsif @word.include? guess
+      @guesses << guess 
+    else
+      @wrong_guesses << guess  
+    end
+
+  end
+  def word_with_guesses
+    new = ''
+    @word.each_char do | u|
+      if @guesses.include? u
+        new << u
+      else
+      new << '-' #unless  @wrong_guesses.include? u
+    end
+    end
+    return new
+  end
+  def check_win_or_lose
+    if @guesses.length + @wrong_guesses.length >=7
+      return :lose
+    end
+    new = ''
+    @word.each_char do | u|
+      if @guesses.include? u
+        new << ''
+      else
+        new << u
+      end
+    end
+    if new.empty?
+      return :win
+    
+  else
+    return :play
+  end
+  end
 end
